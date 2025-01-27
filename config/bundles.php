ba43d1b6782697cc0140c43951c16c7aad3e41a4
Patrick Hagemeister <github@hagemeister.cc>
2025-01-27 14:55:11 +0100

    Add quill in bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
+    PimcoreQuillBundle::class => ['all' => true],
 ];
